0b2664d35807a0c41883d6366e58537c623ebe56
codev-kuk-mac <kuk@codev.kr>
2019-05-09 12:16:45 +0900

    class 이름 입력하도록 코드 수정

--- saveasjpg.jsx
+++ saveasjpg.jsx
@@ -2,19 +2,47 @@
 var layer_list = app.activeDocument.layers;
 var layer_length = layer_list.length;
 var input_name = app.activeDocument.name;
-var output_name = input_name.split('.')[0]+'_leaf_';
+var class_list = ["car"];
+var class_name = "";
+var flag = false;
 
-layer_list.getByName('레이어 0').visible = false;
+while(true){
+	class_name = prompt("Please enter your name", "");
+	if ( class_name == null) {
+	  alert("User cancelled the prompt.");
+	  break;
+	} 
 
-//흰색 부분 전체 투명화
-for (var i = 2; i < layer_length; i++) {
-	layer_list.getByName('레이어 '+i).visible = false;
+	for (var i = class_list.length - 1; i >= 0; i--) {
+		if(class_list[i] == class_name){
+			flag = true;
+			alert("class name is "+class_name);
+			break;
+		}
+	}
+
+	if( flag ) {
+	  break;
+	} else {
+		alert("unavaluable class name");
+	}
 }
 
-for (var i = 2; i < layer_length; i++) {
-	layer_list.getByName('레이어 '+i).visible = true;
-	saveJpeg(output_name+(i-2));
-	layer_list.getByName('레이어 '+i).visible = false;
+if(flag){
+	var output_name = input_name.split('.')[0]+'_class_name_';
+
+	layer_list.getByName('레이어 0').visible = false;
+
+	//흰색 부분 전체 투명화
+	for (var i = 2; i < layer_length; i++) {
+		layer_list.getByName('레이어 '+i).visible = false;
+	}
+
+	for (var i = 2; i < layer_length; i++) {
+		layer_list.getByName('레이어 '+i).visible = true;
+		saveJpeg(output_name+(i-2));
+		layer_list.getByName('레이어 '+i).visible = false;
+	}
 }
 
 function saveJpeg(name){
